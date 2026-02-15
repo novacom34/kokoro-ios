e80050f7576b9f2d7a0378a017800a527a8b32f1
Roma <novacom34@gmail.com>
2026-02-15 18:15:17 +0200

    Update MisakiSwift dependency to use main branch

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/ml-explore/mlx-swift", from: "0.30.2"),
     // .package(url: "https://github.com/mlalma/eSpeakNGSwift", from: "1.0.1"),
-    .package(url: "https://github.com/novacom34/MisakiSwift", from: "1.0.6"),
+    .package(url: "https://github.com/novacom34/MisakiSwift", branch: "main"),
     .package(url: "https://github.com/mlalma/MLXUtilsLibrary.git", from: "0.0.6")
   ],
   targets: [
